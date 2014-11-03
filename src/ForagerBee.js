11b9b00a878e1f73bb7133552bc6d9e383de315a
Alex Hawkins <alexhawkins.me@gmail.com>
2014-11-03 12:31:59 -0800

    Implement Pseudoclassical Inheritance for Bee taxonomy

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+
+//a forrage method that allows the bee to add a treasure to the treasureChest
+ForagerBee.prototype.forage = function(treasure){
+  this.treasureChest.push(treasure);
+};
